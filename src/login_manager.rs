baaa7cc05d97dc0b5e18a79c2e2c2aba2a89c940
kernelkind <kernelkind@gmail.com>
2024-04-27 12:12:38 -0400

    move login logic from promise to async fns
    Signed-off-by: kernelkind <kernelkind@gmail.com>
    Signed-off-by: William Casarin <jb55@jb55.com>

--- src/login_manager.rs
+++ src/login_manager.rs
@@ -4,12 +4,11 @@ use egui::{TextBuffer, TextEdit};
 use nostr_sdk::Keys;
 use poll_promise::Promise;
 
-/// Helper storage object for retrieving the plaintext key from the user and converting it into a
-/// nostr-sdk Keys object if possible.
+/// The UI view interface to log in to a nostr account.
 #[derive(Default)]
 pub struct LoginManager {
     login_key: String,
-    promise: Option<Promise<Result<Keys, LoginError>>>,
+    promise_query: Option<(String, Promise<Result<Keys, LoginError>>)>,
     error: Option<LoginError>,
     key_on_error: Option<String>,
 }
@@ -18,12 +17,13 @@ impl<'a> LoginManager {
     pub fn new() -> Self {
         LoginManager {
             login_key: String::new(),
-            promise: None,
+            promise_query: None,
             error: None,
             key_on_error: None,
         }
     }
 
+    /// Get the textedit for the login UI without exposing the key variable
     pub fn get_login_textedit(
         &'a mut self,
         textedit_closure: fn(&'a mut dyn TextBuffer) -> TextEdit<'a>,
@@ -31,14 +31,30 @@ impl<'a> LoginManager {
         textedit_closure(&mut self.login_key)
     }
 
+    /// User pressed the 'login' button
     pub fn apply_login(&'a mut self) {
-        self.promise = Some(perform_key_retrieval(&self.login_key));
+        let new_promise = match &self.promise_query {
+            Some((query, _)) => {
+                if query != &self.login_key {
+                    Some(perform_key_retrieval(&self.login_key))
+                } else {
+                    None
+                }
+            }
+            None => Some(perform_key_retrieval(&self.login_key)),
+        };
+
+        if let Some(new_promise) = new_promise {
+            self.promise_query = Some((self.login_key.clone(), new_promise));
+        }
     }
 
+    /// Whether to indicate to the user that there is a network operation occuring
     pub fn is_awaiting_network(&self) -> bool {
-        self.promise.is_some()
+        self.promise_query.is_some()
     }
 
+    /// Whether to indicate to the user that a login error occured
     pub fn check_for_error(&'a mut self) -> Option<&'a LoginError> {
         if let Some(error_key) = &self.key_on_error {
             if self.login_key != *error_key {
@@ -50,10 +66,11 @@ impl<'a> LoginManager {
         self.error.as_ref()
     }
 
+    /// Whether to indicate to the user that a successful login occured
     pub fn check_for_successful_login(&mut self) -> Option<Keys> {
-        if let Some(promise) = &mut self.promise {
+        if let Some((_, promise)) = &mut self.promise_query {
             if promise.ready().is_some() {
-                if let Some(promise) = self.promise.take() {
+                if let Some((_, promise)) = self.promise_query.take() {
                     match promise.block_and_take() {
                         Ok(key) => {
                             return Some(key);
@@ -76,10 +93,9 @@ mod tests {
     use nostr_sdk::PublicKey;
     use std::time::{Duration, Instant};
 
-    #[test]
-    fn test_retrieve_key() {
+    #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
+    async fn test_retrieve_key() {
         let mut manager = LoginManager::new();
-        let manager_ref = &mut manager;
         let expected_str = "3efdaebb1d8923ebd99c9e7ace3b4194ab45512e2be79c1b7d68d9243e0d2681";
         let expected_key = Keys::from_public_key(PublicKey::from_hex(expected_str).unwrap());
 
@@ -89,24 +105,37 @@ mod tests {
             let cur_time = start_time.elapsed();
 
             if cur_time < Duration::from_millis(10u64) {
-                let key = "test";
-                manager_ref.login_key = String::from(key);
-                manager_ref.promise = Some(perform_key_retrieval(key));
+                let _ = manager.get_login_textedit(|text| {
+                    text.clear();
+                    text.insert_text("test", 0);
+                    egui::TextEdit::singleline(text)
+                });
+                manager.apply_login();
             } else if cur_time < Duration::from_millis(30u64) {
-                let key = "test2";
-                manager_ref.login_key = String::from(key);
-                manager_ref.promise = Some(perform_key_retrieval(key));
+                let _ = manager.get_login_textedit(|text| {
+                    text.clear();
+                    text.insert_text("test2", 0);
+                    egui::TextEdit::singleline(text)
+                });
+                manager.apply_login();
             } else {
-                manager_ref.login_key = String::from(expected_str);
-                manager_ref.promise = Some(perform_key_retrieval(expected_str));
+                let _ = manager.get_login_textedit(|text| {
+                    text.clear();
+                    text.insert_text(
+                        "3efdaebb1d8923ebd99c9e7ace3b4194ab45512e2be79c1b7d68d9243e0d2681",
+                        0,
+                    );
+                    egui::TextEdit::singleline(text)
+                });
+                manager.apply_login();
             }
 
-            if let Some(key) = manager_ref.check_for_successful_login() {
+            if let Some(key) = manager.check_for_successful_login() {
                 assert_eq!(expected_key, key);
                 return;
             }
         }
 
-        panic!();
+        panic!("Test failed to get expected key.");
     }
 }
