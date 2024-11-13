9801a204290a6aaacd806e7f76f90610fa07594b
William Casarin <jb55@jb55.com>
2024-11-13 10:51:51 -0800

    storage: simplify imports
    Signed-off-by: William Casarin <jb55@jb55.com>

--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -2,9 +2,7 @@ mod file_key_storage;
 mod file_storage;
 
 pub use file_key_storage::FileKeyStorage;
-pub use file_storage::write_file;
-pub use file_storage::Directory;
-pub use file_storage::{DataPath, DataPathType};
+pub use file_storage::{delete_file, write_file, DataPath, DataPathType, Directory};
 
 #[cfg(target_os = "macos")]
 mod security_framework_key_storage;
