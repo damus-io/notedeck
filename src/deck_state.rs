b2d92e7160e6ce7ab5627f45d6e2408b1fa15e0b
kernelkind <kernelkind@gmail.com>
2024-11-17 18:20:10 -0500

    configure deck UI update
    Signed-off-by: kernelkind <kernelkind@gmail.com>

--- src/deck_state.rs
+++ src/deck_state.rs
@@ -1,11 +1,14 @@
-use crate::decks::Deck;
+use crate::{app_style::emoji_font_family, decks::Deck};
 
 /// State for UI creating/editing deck
-#[derive(Default)]
 pub struct DeckState {
     pub deck_name: String,
     pub selected_glyph: Option<char>,
     pub deleting: bool,
+    pub selecting_glyph: bool,
+    pub warn_no_title: bool,
+    pub warn_no_icon: bool,
+    glyph_options: Option<Vec<char>>,
 }
 
 impl DeckState {
@@ -20,11 +23,43 @@ impl DeckState {
         Self {
             deck_name,
             selected_glyph,
-            deleting: false,
+            ..Default::default()
         }
     }
 
     pub fn clear(&mut self) {
         *self = Default::default();
     }
+
+    pub fn get_glyph_options(&mut self, ui: &egui::Ui) -> &Vec<char> {
+        self.glyph_options
+            .get_or_insert_with(|| available_characters(ui, emoji_font_family()))
+    }
+}
+
+impl Default for DeckState {
+    fn default() -> Self {
+        Self {
+            deck_name: Default::default(),
+            selected_glyph: Default::default(),
+            deleting: Default::default(),
+            selecting_glyph: true,
+            warn_no_icon: Default::default(),
+            warn_no_title: Default::default(),
+            glyph_options: Default::default(),
+        }
+    }
+}
+
+fn available_characters(ui: &egui::Ui, family: egui::FontFamily) -> Vec<char> {
+    ui.fonts(|f| {
+        f.lock()
+            .fonts
+            .font(&egui::FontId::new(10.0, family)) // size is arbitrary for getting the characters
+            .characters()
+            .iter()
+            .filter(|chr| !chr.is_whitespace() && !chr.is_ascii_control())
+            .copied()
+            .collect()
+    })
 }
--- src/ui/configure_deck.rs
+++ src/ui/configure_deck.rs
@@ -2,6 +2,7 @@ use egui::{vec2, Button, Color32, Label, RichText, Ui, Widget};
 
 use crate::{
     app_style::{deck_icon_font_sized, get_font_size, NotedeckTextStyle, DECK_ICON_SIZE},
+    colors::PINK,
     deck_state::DeckState,
     fonts::NamedFontFamily,
 };
@@ -48,45 +49,110 @@ impl<'a> ConfigureDeckView<'a> {
             ui.add_space(8.0);
             ui.text_edit_singleline(&mut self.state.deck_name);
             ui.add_space(8.0);
-            ui.label("We recommend short names");
+            ui.add(Label::new(
+                RichText::new("We recommend short names")
+                    .color(ui.visuals().noninteractive().fg_stroke.color)
+                    .size(get_font_size(ui.ctx(), &NotedeckTextStyle::Small)),
+            ));
 
             ui.add_space(32.0);
             ui.add(Label::new(RichText::new("Icon").font(title_font)));
 
-            if self.state.selected_glyph.is_none() {
-                self.state.selected_glyph = Some('A'); // TODO: get user's selection instead of hard coding
+            if ui
+                .add(deck_icon(
+                    ui.id().with("config-deck"),
+                    self.state.selected_glyph,
+                    64.0,
+                    false,
+                ))
+                .clicked()
+            {
+                self.state.selecting_glyph = !self.state.selecting_glyph;
             }
 
-            ui.add(deck_icon(
-                ui.id().with("config-deck"),
-                self.state.selected_glyph,
-                64.0,
-                false,
-            ));
+            if self.state.selecting_glyph {
+                let max_height = if ui.available_height() - 100.0 > 0.0 {
+                    ui.available_height() - 100.0
+                } else {
+                    ui.available_height()
+                };
+                egui::Frame::window(ui.style()).show(ui, |ui| {
+                    let glyphs = self.state.get_glyph_options(ui);
+                    if let Some(selected_glyph) = glyph_options_ui(ui, 16.0, max_height, glyphs) {
+                        self.state.selected_glyph = Some(selected_glyph);
+                        self.state.selecting_glyph = false;
+                    }
+                });
+                ui.add_space(16.0);
+            }
 
+            if self.state.warn_no_icon && self.state.selected_glyph.is_some() {
+                self.state.warn_no_icon = false;
+            }
+            if self.state.warn_no_title && !self.state.deck_name.is_empty() {
+                self.state.warn_no_title = false;
+            }
+
+            show_warnings(ui, self.state.warn_no_icon, self.state.warn_no_title);
+
+            let mut resp = None;
             if ui
                 .add(create_deck_button(&self.create_button_text))
                 .clicked()
             {
-                if let Some(glyph) = self.state.selected_glyph {
-                    Some(ConfigureDeckResponse {
-                        icon: glyph,
-                        name: self.state.deck_name.clone(),
-                    })
-                } else {
-                    // TODO: error message saying to select glyph
-                    None
+                if self.state.deck_name.is_empty() {
+                    self.state.warn_no_title = true;
                 }
-            } else {
-                None
+                if self.state.selected_glyph.is_none() {
+                    self.state.warn_no_icon = true;
+                }
+                if !self.state.deck_name.is_empty() {
+                    if let Some(glyph) = self.state.selected_glyph {
+                        resp = Some(ConfigureDeckResponse {
+                            icon: glyph,
+                            name: self.state.deck_name.clone(),
+                        });
+                    }
+                }
             }
+            resp
         })
         .inner
     }
 }
 
-fn create_deck_button(text: &str) -> impl Widget {
-    Button::new(text)
+fn show_warnings(ui: &mut Ui, warn_no_icon: bool, warn_no_title: bool) {
+    if warn_no_icon || warn_no_title {
+        let messages = [
+            if warn_no_title {
+                "create a name for the deck"
+            } else {
+                ""
+            },
+            if warn_no_icon { "select an icon" } else { "" },
+        ];
+        let message = messages
+            .iter()
+            .filter(|&&m| !m.is_empty())
+            .copied()
+            .collect::<Vec<_>>()
+            .join(" and ");
+
+        ui.add(
+            egui::Label::new(RichText::new(format!("Please {}.", message)).color(Color32::RED))
+                .wrap(),
+        );
+    }
+}
+
+fn create_deck_button(text: &str) -> impl Widget + use<'_> {
+    move |ui: &mut egui::Ui| {
+        let size = vec2(108.0, 40.0);
+        ui.allocate_ui_with_layout(size, egui::Layout::top_down(egui::Align::Center), |ui| {
+            ui.add(Button::new(text).fill(PINK).min_size(size))
+        })
+        .inner
+    }
 }
 
 pub fn deck_icon(
@@ -132,6 +198,85 @@ pub fn deck_icon(
 
         helper.take_animation_response()
     }
+}
+
+fn glyph_icon_max_size(ui: &egui::Ui, glyph: &char, font_size: f32) -> egui::Vec2 {
+    let painter = ui.painter();
+    let font = deck_icon_font_sized(font_size * ICON_EXPANSION_MULTIPLE);
+    let glyph_galley = painter.layout_no_wrap(glyph.to_string(), font, Color32::WHITE);
+    glyph_galley.rect.size()
+}
+
+fn glyph_icon(glyph: char, font_size: f32, max_size: egui::Vec2) -> impl Widget {
+    move |ui: &mut egui::Ui| {
+        let helper = AnimationHelper::new(ui, ("glyph", glyph), max_size);
+        let painter = ui.painter_at(helper.get_animation_rect());
+
+        let font = deck_icon_font_sized(helper.scale_1d_pos(font_size));
+        let glyph_galley = painter.layout_no_wrap(glyph.to_string(), font, Color32::WHITE);
+
+        let top_left = {
+            let mut glyph_rect = glyph_galley.rect;
+            glyph_rect.set_center(helper.get_animation_rect().center());
+            glyph_rect.left_top()
+        };
+
+        painter.galley(top_left, glyph_galley, Color32::WHITE);
+        helper.take_animation_response()
+    }
+}
+
+fn glyph_options_ui(
+    ui: &mut egui::Ui,
+    font_size: f32,
+    max_height: f32,
+    glyphs: &[char],
+) -> Option<char> {
+    let mut selected_glyph = None;
+    egui::ScrollArea::vertical()
+        .max_height(max_height)
+        .show(ui, |ui| {
+            let max_width = ui.available_width();
+            let mut row_glyphs = Vec::new();
+            let mut cur_width = 0.0;
+            let spacing = ui.spacing().item_spacing.x;
+
+            for (index, glyph) in glyphs.iter().enumerate() {
+                let next_glyph_size = glyph_icon_max_size(ui, glyph, font_size);
+
+                if cur_width + spacing + next_glyph_size.x > max_width {
+                    if let Some(selected) = paint_row(ui, &row_glyphs, font_size) {
+                        selected_glyph = Some(selected);
+                    }
+                    row_glyphs.clear();
+                    cur_width = 0.0;
+                }
+
+                cur_width += spacing;
+                cur_width += next_glyph_size.x;
+                row_glyphs.push(*glyph);
+
+                if index == glyphs.len() - 1 {
+                    if let Some(selected) = paint_row(ui, &row_glyphs, font_size) {
+                        selected_glyph = Some(selected);
+                    }
+                }
+            }
+        });
+    selected_glyph
+}
+
+fn paint_row(ui: &mut egui::Ui, row_glyphs: &[char], font_size: f32) -> Option<char> {
+    let mut selected_glyph = None;
+    ui.horizontal(|ui| {
+        for glyph in row_glyphs {
+            let glyph_size = glyph_icon_max_size(ui, glyph, font_size);
+            if ui.add(glyph_icon(*glyph, font_size, glyph_size)).clicked() {
+                selected_glyph = Some(*glyph);
+            }
+        }
+    });
+    selected_glyph
 }
 
 mod preview {
