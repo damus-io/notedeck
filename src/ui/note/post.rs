damus-io/notedeck

commit 8464a1d22c68d185420d7204c1314bcde7ae18c7

disable post button if draft buffer empty
closes: https://github.com/damus-io/notedeck/issues/417

Signed-off-by: kernelkind <kernelkind@gmail.com>

src/ui/note/post.rs
@@ -209,7 +209,10 @@ impl<'a> PostView<'a> {
 
                             ui.with_layout(egui::Layout::right_to_left(egui::Align::BOTTOM), |ui| {
                                 if ui
-                                    .add_sized([91.0, 32.0], egui::Button::new("Post now"))
+                                    .add_sized(
+                                        [91.0, 32.0],
+                                        post_button(!self.draft.buffer.is_empty()),
+                                    )
                                     .clicked()
                                 {
                                     let new_post = NewPost::new(
@@ -233,6 +236,23 @@ impl<'a> PostView<'a> {
                 .inner
             })
             .inner
+    }
+}
+
+fn post_button(interactive: bool) -> impl egui::Widget {
+    move |ui: &mut egui::Ui| {
+        let button = egui::Button::new("Post now");
+        if interactive {
+            ui.add(button)
+        } else {
+            ui.add(
+                button
+                    .sense(egui::Sense::hover())
+                    .fill(ui.visuals().widgets.noninteractive.bg_fill)
+                    .stroke(ui.visuals().widgets.noninteractive.bg_stroke),
+            )
+            .on_hover_cursor(egui::CursorIcon::NotAllowed)
+        }
     }
 }
 

commit 4133570c2e2e8c7ab03d3edec2f69b4f392cde18

ui: simply hide post button if buffer is empty
Fixes: 8464a1d22c68 ("disable post button if draft buffer empty")
Signed-off-by: William Casarin <jb55@jb55.com>

src/ui/note/post.rs
@@ -208,10 +208,15 @@ impl<'a> PostView<'a> {
                             }
 
                             ui.with_layout(egui::Layout::right_to_left(egui::Align::BOTTOM), |ui| {
+                                if self.draft.buffer.is_empty() {
+                                    // Don't render button if our buffer is empty
+                                    return None;
+                                }
+
                                 if ui
                                     .add_sized(
                                         [91.0, 32.0],
-                                        post_button(!self.draft.buffer.is_empty()),
+                                        egui::Button::new("Post now")
                                     )
                                     .clicked()
                                 {
@@ -236,23 +241,6 @@ impl<'a> PostView<'a> {
                 .inner
             })
             .inner
-    }
-}
-
-fn post_button(interactive: bool) -> impl egui::Widget {
-    move |ui: &mut egui::Ui| {
-        let button = egui::Button::new("Post now");
-        if interactive {
-            ui.add(button)
-        } else {
-            ui.add(
-                button
-                    .sense(egui::Sense::hover())
-                    .fill(ui.visuals().widgets.noninteractive.bg_fill)
-                    .stroke(ui.visuals().widgets.noninteractive.bg_stroke),
-            )
-            .on_hover_cursor(egui::CursorIcon::NotAllowed)
-        }
     }
 }
 