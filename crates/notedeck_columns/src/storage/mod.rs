4f89d95aefb3f302b6558d15682008396b491f33
William Casarin <jb55@jb55.com>
2025-01-05 11:50:08 -0600

    token_serializer: introduce TokenWriter
    This simplifies token serialization
    
    Signed-off-by: William Casarin <jb55@jb55.com>

--- crates/notedeck_columns/src/storage/mod.rs
+++ crates/notedeck_columns/src/storage/mod.rs
@@ -5,4 +5,4 @@ mod token_parser;
 pub use decks::{load_decks_cache, save_decks_cache, DECKS_CACHE_FILE};
 pub use migration::{deserialize_columns, COLUMNS_FILE};
 
-pub use token_parser::{ParseError, TokenParser, TokenSerializable};
+pub use token_parser::{ParseError, TokenParser, TokenSerializable, TokenWriter};
