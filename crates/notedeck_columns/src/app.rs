0249b5ab04af69a2b44c62d3971eef4e007ffc70
kieran <kieran@harkin.me>
2025-01-21 10:29:22 +0000

    Always update accounts

--- crates/notedeck_columns/src/app.rs
+++ crates/notedeck_columns/src/app.rs
@@ -183,8 +183,6 @@ fn unknown_id_send(unknown_ids: &mut UnknownIds, pool: &mut RelayPool) {
 }
 
 fn update_damus(damus: &mut Damus, app_ctx: &mut AppContext<'_>, ctx: &egui::Context) {
-    app_ctx.accounts.update(app_ctx.ndb, app_ctx.pool, ctx); // update user relay and mute lists
-
     match damus.state {
         DamusState::Initializing => {
             damus.state = DamusState::Initialized;
