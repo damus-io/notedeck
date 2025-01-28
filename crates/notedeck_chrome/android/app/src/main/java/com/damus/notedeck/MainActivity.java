d174b141f6848e98ba93887aa5ba272e3885ce76
William Casarin <jb55@jb55.com>
2025-01-28 13:35:43 -0800

    android: get GameActivity to launch
    For some reason there are no touch inputs though
    
    Signed-off-by: William Casarin <jb55@jb55.com>

--- crates/notedeck_chrome/android/app/src/main/java/com/damus/notedeck/MainActivity.java
+++ crates/notedeck_chrome/android/app/src/main/java/com/damus/notedeck/MainActivity.java
@@ -16,7 +16,7 @@ import com.google.androidgamesdk.GameActivity;
 
 public class MainActivity extends GameActivity {
   static {
-    System.loadLibrary("main");
+    System.loadLibrary("notedeck_chrome");
   }
 
   @Override
