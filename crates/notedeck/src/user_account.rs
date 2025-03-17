28b815825fbde5c8881687577229fba2725d8d84
kernelkind <kernelkind@gmail.com>
2025-03-17 19:23:50 -0400

    serialize `UserAccount`
    Signed-off-by: kernelkind <kernelkind@gmail.com>

--- crates/notedeck/src/user_account.rs
+++ crates/notedeck/src/user_account.rs
@@ -1,4 +1,5 @@
 use enostr::Keypair;
+use tokenator::TokenSerializable;
 
 pub struct UserAccount {
     pub key: Keypair,
@@ -7,5 +8,17 @@ pub struct UserAccount {
 impl UserAccount {
     pub fn new(key: Keypair) -> Self {
         Self { key }
+    }
+}
+
+impl TokenSerializable for UserAccount {
+    fn parse_from_tokens<'a>(
+        parser: &mut tokenator::TokenParser<'a>,
+    ) -> Result<Self, tokenator::ParseError<'a>> {
+        Ok(UserAccount::new(Keypair::parse_from_tokens(parser)?))
+    }
+
+    fn serialize_tokens(&self, writer: &mut tokenator::TokenWriter) {
+        self.key.serialize_tokens(writer);
     }
 }
