1e9c38b6f9ed3ab6584d5a97eb8ff6a66227eec7
kernelkind <kernelkind@gmail.com>
2025-03-18 15:41:03 -0400

    make `Wallet` `TokenSerializable`
    Signed-off-by: kernelkind <kernelkind@gmail.com>

--- crates/notedeck/src/wallet.rs
+++ crates/notedeck/src/wallet.rs
@@ -5,6 +5,7 @@ use nwc::{
     NWC,
 };
 use poll_promise::Promise;
+use tokenator::TokenSerializable;
 use tokio::sync::RwLock;
 
 use crate::{Accounts, Job, JobId, Jobs};
@@ -146,13 +147,57 @@ fn save_uri(accounts: &mut Accounts) {
         acc.wallet_state = WalletState::Wallet(wallet);
     });
 }
+
+impl TokenSerializable for Wallet {
+    fn parse_from_tokens<'a>(
+        parser: &mut tokenator::TokenParser<'a>,
+    ) -> Result<Self, tokenator::ParseError<'a>> {
+        parser.parse_all(|p| {
+            p.parse_token("nwc_uri")?;
+
+            let raw_uri = p.pull_token()?;
+
+            let wallet =
+                Wallet::new(raw_uri.to_owned()).map_err(|_| tokenator::ParseError::DecodeFailed)?;
+
+            Ok(wallet)
+        })
+    }
+
+    fn serialize_tokens(&self, writer: &mut tokenator::TokenWriter) {
+        writer.write_token("nwc_uri");
+        writer.write_token(&self.uri);
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use tokenator::{TokenParser, TokenSerializable, TokenWriter};
+
     use crate::Wallet;
     const URI: &str = "nostr+walletconnect://b889ff5b1513b641e2a139f661a661364979c5beee91842f8f0ef42ab558e9d4?relay=wss%3A%2F%2Frelay.damus.io&secret=71a8c14c1407c113601079c4302dab36460f0ccd0ad506f1f2dc73b5100e4f3c&lud16=nostr%40nostr.com";
 
     #[test]
     fn test_uri() {
         assert!(Wallet::new(URI.to_owned()).is_ok())
+    }
+
+    #[test]
+    fn test_wallet_serialize_deserialize() {
+        let wallet = Wallet::new(URI.to_owned()).unwrap();
+
+        let mut writer = TokenWriter::new("\t");
+        wallet.serialize_tokens(&mut writer);
+        let serialized = writer.str();
+
+        let data = &serialized.split("\t").collect::<Vec<&str>>();
+        let mut parser = TokenParser::new(data);
+        let m_new_wallet = Wallet::parse_from_tokens(&mut parser);
+
+        assert!(m_new_wallet.is_ok());
+
+        let new_wallet = m_new_wallet.unwrap();
+
+        assert_eq!(wallet.uri, new_wallet.uri);
     }
 }
