7f4cefd01545b62cec3d951f2ddd287ea543a2c2
kernelkind <kernelkind@gmail.com>
2025-03-19 17:28:28 -0400

    add nwc jobs
    Signed-off-by: kernelkind <kernelkind@gmail.com>

--- crates/notedeck/src/jobs.rs
+++ crates/notedeck/src/jobs.rs
@@ -26,6 +26,8 @@ impl std::hash::Hash for JobId<'_> {
     fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
         match self {
             JobId::Blurhash(s) => s.hash(state),
+            JobId::NWCBalance(s) => s.hash(state),
+            JobId::NWCInvoice(s) => s.hash(state),
         }
     }
 }
@@ -34,6 +36,8 @@ impl std::hash::Hash for JobIdOwned {
     fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
         match self {
             JobIdOwned::Blurhash(s) => s.hash(state),
+            JobIdOwned::NWCBalance(s) => s.hash(state),
+            JobIdOwned::NWCInvoice(s) => s.hash(state),
         }
     }
 }
@@ -41,6 +45,8 @@ impl std::hash::Hash for JobIdOwned {
 fn to_owned(jobid: &JobId) -> JobIdOwned {
     match jobid {
         JobId::Blurhash(s) => JobIdOwned::Blurhash(s.to_string()),
+        JobId::NWCBalance(s) => JobIdOwned::NWCBalance(s.to_string()),
+        JobId::NWCInvoice(s) => JobIdOwned::NWCInvoice(s.to_string()),
     }
 }
 
@@ -48,6 +54,9 @@ impl hashbrown::Equivalent<JobIdOwned> for JobId<'_> {
     fn equivalent(&self, key: &JobIdOwned) -> bool {
         match (self, key) {
             (JobId::Blurhash(a), JobIdOwned::Blurhash(b)) => *a == b.as_str(),
+            (JobId::NWCBalance(a), JobIdOwned::NWCBalance(b)) => *a == b.as_str(),
+            (JobId::NWCInvoice(a), JobIdOwned::NWCInvoice(b)) => *a == b.as_str(),
+            (_, _) => false,
         }
     }
 }
@@ -55,20 +64,30 @@ impl hashbrown::Equivalent<JobIdOwned> for JobId<'_> {
 #[derive(Debug, PartialEq, Eq, Clone)]
 enum JobIdOwned {
     Blurhash(String), // image URL
+    #[allow(dead_code)]
+    NWCBalance(String), // Wallet's URI
+    #[allow(dead_code)]
+    NWCInvoice(String), // LN invoice
 }
 
 pub enum JobId<'a> {
-    Blurhash(&'a str), // image URL
+    Blurhash(&'a str),   // image URL
+    NWCBalance(&'a str), // Wallet's URI
+    NWCInvoice(&'a str), // LN invoice
 }
 
 pub enum Job {
     ProcessBlurhash(Promise<Option<TextureHandle>>),
+    GetNWCBalance(Promise<Result<u64, nwc::Error>>),
+    PayNWCInvoice(Promise<Result<nwc::nostr::nips::nip47::PayInvoiceResponse, nwc::Error>>),
 }
 
 impl std::fmt::Debug for Job {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         match self {
             Job::ProcessBlurhash(_) => write!(f, "ProcessBlurhash"),
+            Job::GetNWCBalance(_) => write!(f, "GetNWCBalance"),
+            Job::PayNWCInvoice(_) => write!(f, "PayNWCInvoice"),
         }
     }
 }
