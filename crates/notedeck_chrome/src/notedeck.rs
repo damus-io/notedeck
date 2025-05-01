10a2459da201eff8f8ca0040d630e181d3e3ea56
William Casarin <jb55@jb55.com>
2025-05-01 12:03:36 -0700

    windows: don't show terminal window
    Looks like this got accidentally commented out in an android build
    
    Signed-off-by: William Casarin <jb55@jb55.com>

--- crates/notedeck_chrome/src/notedeck.rs
+++ crates/notedeck_chrome/src/notedeck.rs
@@ -1,4 +1,4 @@
-//#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 // hide console window on Windows in release
 
 use notedeck::{DataPath, DataPathType, Notedeck};
