94be9ccc3e3b43e886a28ecc62f6950afcadb7a0
William Casarin <jb55@jb55.com>
2025-07-20 15:51:19 -0700

    Revert "relay: make multicast a desired relay"
    there seems to be a bug where the relay list fails to get
    created if multicast fails to connect
    
    This reverts commit 5eae9a55eca685071a116f64a424f5be222cb467.

--- crates/notedeck/src/account/relay.rs
+++ crates/notedeck/src/account/relay.rs
@@ -137,7 +137,6 @@ impl RelayDefaults {
             .map(|u| RelaySpec::new(AccountRelayData::canonicalize_url(&u), false, false))
             .collect();
         let bootstrap_relays = [
-            "multicast",
             "wss://relay.damus.io",
             // "wss://pyramid.fiatjaf.com",  // Uncomment if needed
             "wss://nos.lol",
