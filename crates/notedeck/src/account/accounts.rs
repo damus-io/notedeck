damus-io/notedeck

commit 397bfce8173caa7226551ad80988a26da896ec8d

add `Accounts` to `NoteContext`
Signed-off-by: kernelkind <kernelkind@gmail.com>

crates/notedeck/src/note/mod.rs
@@ -4,6 +4,7 @@ mod context;
 pub use action::{MediaAction, NoteAction, ScrollInfo, ZapAction, ZapTargetAmount};
 pub use context::{BroadcastContext, ContextSelection, NoteContextSelection};
 
+use crate::Accounts;
 use crate::JobPool;
 use crate::UnknownIds;
 use crate::{notecache::NoteCache, zaps::Zaps, Images};
@@ -17,6 +18,7 @@ use std::fmt;
 /// passed to inner UI elements, minimizing prop drilling.
 pub struct NoteContext<'d> {
     pub ndb: &'d Ndb,
+    pub accounts: &'d Accounts,
     pub img_cache: &'d mut Images,
     pub note_cache: &'d mut NoteCache,
     pub zaps: &'d mut Zaps,

crates/notedeck_columns/src/nav.rs
@@ -424,8 +424,10 @@ fn render_nav_body(
     col: usize,
     inner_rect: egui::Rect,
 ) -> Option<RenderNavAction> {
+    let current_account_has_wallet = get_current_wallet(ctx.accounts, ctx.global_wallet).is_some();
     let mut note_context = NoteContext {
         ndb: ctx.ndb,
+        accounts: ctx.accounts,
         img_cache: ctx.img_cache,
         note_cache: ctx.note_cache,
         zaps: ctx.zaps,
@@ -433,12 +435,11 @@ fn render_nav_body(
         job_pool: ctx.job_pool,
         unknown_ids: ctx.unknown_ids,
         clipboard: ctx.clipboard,
-        current_account_has_wallet: get_current_wallet(ctx.accounts, ctx.global_wallet).is_some(),
+        current_account_has_wallet,
     };
     match top {
         Route::Timeline(kind) => render_timeline_route(
             &mut app.timeline_cache,
-            ctx.accounts,
             kind,
             col,
             app.note_options,
@@ -449,7 +450,6 @@ fn render_nav_body(
         ),
         Route::Thread(selection) => render_thread_route(
             &mut app.threads,
-            ctx.accounts,
             selection,
             col,
             app.note_options,
@@ -602,7 +602,6 @@ fn render_nav_body(
                 app.note_options,
                 search_buffer,
                 &mut note_context,
-                &(&ctx.accounts.get_selected_account().key).into(),
                 &mut app.jobs,
             )
             .show(ui)

crates/notedeck_columns/src/ui/note/post.rs
@@ -12,7 +12,7 @@ use egui::{
     widgets::text_edit::TextEdit,
     Frame, Layout, Margin, Pos2, ScrollArea, Sense, TextBuffer,
 };
-use enostr::{FilledKeypair, FullKeypair, KeypairUnowned, NoteId, Pubkey, RelayPool};
+use enostr::{FilledKeypair, FullKeypair, NoteId, Pubkey, RelayPool};
 use nostrdb::{Ndb, Transaction};
 use notedeck_ui::{
     app_images,
@@ -352,15 +352,9 @@ impl<'a, 'd> PostView<'a, 'd> {
                             ui.vertical(|ui| {
                                 ui.set_max_width(avail_size.x * 0.8);
 
-                                let zapping_acc = self
-                                    .note_context
-                                    .current_account_has_wallet
-                                    .then(|| KeypairUnowned::from(&self.poster));
-
                                 render_note_preview(
                                     ui,
                                     self.note_context,
-                                    zapping_acc.as_ref(),
                                     txn,
                                     id.bytes(),
                                     nostrdb::NoteKey::new(0),
@@ -793,6 +787,7 @@ mod preview {
             let txn = Transaction::new(app.ndb).expect("txn");
             let mut note_context = NoteContext {
                 ndb: app.ndb,
+                accounts: app.accounts,
                 img_cache: app.img_cache,
                 note_cache: app.note_cache,
                 zaps: app.zaps,

crates/notedeck_dave/src/ui/dave.rs
@@ -211,6 +211,7 @@ impl<'a> DaveUi<'a> {
     ) -> Option<NoteAction> {
         let mut note_context = NoteContext {
             ndb: ctx.ndb,
+            accounts: ctx.accounts,
             img_cache: ctx.img_cache,
             note_cache: ctx.note_cache,
             zaps: ctx.zaps,
@@ -243,7 +244,6 @@ impl<'a> DaveUi<'a> {
                                 |ui| {
                                     notedeck_ui::NoteView::new(
                                         &mut note_context,
-                                        None,
                                         &note,
                                         NoteOptions::default(),
                                         jobs,

crates/notedeck_ui/src/note/mod.rs
@@ -33,7 +33,6 @@ use notedeck::{
 
 pub struct NoteView<'a, 'd> {
     note_context: &'a mut NoteContext<'d>,
-    zapping_acc: Option<&'a KeypairUnowned<'a>>,
     parent: Option<NoteKey>,
     note: &'a nostrdb::Note<'a>,
     framed: bool,
@@ -85,7 +84,6 @@ impl egui::Widget for &mut NoteView<'_, '_> {
 impl<'a, 'd> NoteView<'a, 'd> {
     pub fn new(
         note_context: &'a mut NoteContext<'d>,
-        zapping_acc: Option<&'a KeypairUnowned<'a>>,
         note: &'a nostrdb::Note<'a>,
         mut flags: NoteOptions,
         jobs: &'a mut JobsCache,
@@ -98,7 +96,6 @@ impl<'a, 'd> NoteView<'a, 'd> {
 
         Self {
             note_context,
-            zapping_acc,
             parent,
             note,
             flags,
@@ -231,7 +228,6 @@ impl<'a, 'd> NoteView<'a, 'd> {
 
             ui.add(&mut NoteContents::new(
                 self.note_context,
-                self.zapping_acc,
                 txn,
                 self.note,
                 self.flags,
@@ -317,14 +313,7 @@ impl<'a, 'd> NoteView<'a, 'd> {
                     .text_style(style.text_style()),
             );
         });
-        NoteView::new(
-            self.note_context,
-            self.zapping_acc,
-            &note_to_repost,
-            self.flags,
-            self.jobs,
-        )
-        .show(ui)
+        NoteView::new(self.note_context, &note_to_repost, self.flags, self.jobs).show(ui)
     }
 
     pub fn show_impl(&mut self, ui: &mut egui::Ui) -> NoteResponse {
@@ -440,7 +429,6 @@ impl<'a, 'd> NoteView<'a, 'd> {
                         ui.horizontal(|ui| {
                             note_action = reply_desc(
                                 ui,
-                                self.zapping_acc,
                                 txn,
                                 &note_reply,
                                 self.note_context,
@@ -455,32 +443,27 @@ impl<'a, 'd> NoteView<'a, 'd> {
                 })
                 .inner;
 
-            let mut contents = NoteContents::new(
-                self.note_context,
-                self.zapping_acc,
-                txn,
-                self.note,
-                self.flags,
-                self.jobs,
-            );
+            let mut contents =
+                NoteContents::new(self.note_context, txn, self.note, self.flags, self.jobs);
 
             ui.add(&mut contents);
 
             note_action = contents.action.or(note_action);
 
             if self.options().has(NoteOptions::ActionBar) {
-                note_action = render_note_actionbar(
-                    ui,
-                    self.zapping_acc.as_ref().map(|c| Zapper {
+                let zapper = {
+                    let cur_acc = self.note_context.accounts.get_selected_account();
+                    let has_wallet = cur_acc.wallet.is_some();
+
+                    has_wallet.then_some(Zapper {
                         zaps: self.note_context.zaps,
-                        cur_acc: c,
-                    }),
-                    self.note.id(),
-                    self.note.pubkey(),
-                    note_key,
-                )
-                .inner
-                .or(note_action);
+                        cur_acc: cur_acc.keypair(),
+                    })
+                };
+                note_action =
+                    render_note_actionbar(ui, zapper, self.note.id(), self.note.pubkey(), note_key)
+                        .inner
+                        .or(note_action);
             }
 
             NoteUiResponse {
@@ -527,7 +510,6 @@ impl<'a, 'd> NoteView<'a, 'd> {
 
                     note_action = reply_desc(
                         ui,
-                        self.zapping_acc,
                         txn,
                         &note_reply,
                         self.note_context,
@@ -537,25 +519,25 @@ impl<'a, 'd> NoteView<'a, 'd> {
                     .or(note_action.take());
                 });
 
-                let mut contents = NoteContents::new(
-                    self.note_context,
-                    self.zapping_acc,
-                    txn,
-                    self.note,
-                    self.flags,
-                    self.jobs,
-                );
+                let mut contents =
+                    NoteContents::new(self.note_context, txn, self.note, self.flags, self.jobs);
                 ui.add(&mut contents);
 
                 note_action = contents.action.or(note_action);
 
+                let zapper = {
+                    let cur_acc = self.note_context.accounts.get_selected_account();
+                    let has_wallet = cur_acc.wallet.is_some();
+
+                    has_wallet.then_some(Zapper {
+                        zaps: self.note_context.zaps,
+                        cur_acc: cur_acc.keypair(),
+                    })
+                };
                 if self.options().has(NoteOptions::ActionBar) {
                     note_action = render_note_actionbar(
                         ui,
-                        self.zapping_acc.as_ref().map(|c| Zapper {
-                            zaps: self.note_context.zaps,
-                            cur_acc: c,
-                        }),
+                        zapper,
                         self.note.id(),
                         self.note.pubkey(),
                         note_key,
@@ -774,7 +756,7 @@ fn note_hitbox_clicked(
 
 struct Zapper<'a> {
     zaps: &'a Zaps,
-    cur_acc: &'a KeypairUnowned<'a>,
+    cur_acc: KeypairUnowned<'a>,
 }
 
 #[profiling::function]

crates/notedeck_columns/src/ui/profile/mod.rs
@@ -12,7 +12,7 @@ use crate::{
     ui::timeline::{tabs_ui, TimelineTabView},
 };
 use notedeck::{
-    name::get_display_name, profile::get_profile_url, Accounts, IsFollowing, MuteFun, NoteAction,
+    name::get_display_name, profile::get_profile_url, IsFollowing, MuteFun, NoteAction,
     NoteContext, NotedeckTextStyle,
 };
 use notedeck_ui::{
@@ -24,7 +24,6 @@ use notedeck_ui::{
 
 pub struct ProfileView<'a, 'd> {
     pubkey: &'a Pubkey,
-    accounts: &'a Accounts,
     col_id: usize,
     timeline_cache: &'a mut TimelineCache,
     note_options: NoteOptions,
@@ -44,7 +43,6 @@ impl<'a, 'd> ProfileView<'a, 'd> {
     #[allow(clippy::too_many_arguments)]
     pub fn new(
         pubkey: &'a Pubkey,
-        accounts: &'a Accounts,
         col_id: usize,
         timeline_cache: &'a mut TimelineCache,
         note_options: NoteOptions,
@@ -54,7 +52,6 @@ impl<'a, 'd> ProfileView<'a, 'd> {
     ) -> Self {
         ProfileView {
             pubkey,
-            accounts,
             col_id,
             timeline_cache,
             note_options,
@@ -118,7 +115,6 @@ impl<'a, 'd> ProfileView<'a, 'd> {
                 &txn,
                 self.is_muted,
                 self.note_context,
-                &(&self.accounts.get_selected_account().key).into(),
                 self.jobs,
             )
             .show(ui)
@@ -179,7 +175,7 @@ impl<'a, 'd> ProfileView<'a, 'd> {
                         ui.add_space(24.0);
 
                         let target_key = self.pubkey;
-                        let selected = self.accounts.get_selected_account();
+                        let selected = self.note_context.accounts.get_selected_account();
 
                         let profile_type = if selected.key.secret_key.is_none() {
                             ProfileType::ReadOnly

crates/notedeck_columns/src/ui/search/mod.rs
@@ -1,5 +1,5 @@
 use egui::{vec2, Align, Color32, CornerRadius, RichText, Stroke, TextEdit};
-use enostr::{KeypairUnowned, NoteId, Pubkey};
+use enostr::{NoteId, Pubkey};
 use state::TypingType;
 
 use crate::{timeline::TimelineTab, ui::timeline::TimelineTabView};
@@ -27,7 +27,6 @@ pub struct SearchView<'a, 'd> {
     txn: &'a Transaction,
     is_muted: &'a MuteFun,
     note_context: &'a mut NoteContext<'d>,
-    cur_acc: &'a KeypairUnowned<'a>,
     jobs: &'a mut JobsCache,
 }
 
@@ -38,7 +37,6 @@ impl<'a, 'd> SearchView<'a, 'd> {
         note_options: NoteOptions,
         query: &'a mut SearchQueryState,
         note_context: &'a mut NoteContext<'d>,
-        cur_acc: &'a KeypairUnowned<'a>,
         jobs: &'a mut JobsCache,
     ) -> Self {
         Self {
@@ -47,7 +45,6 @@ impl<'a, 'd> SearchView<'a, 'd> {
             query,
             note_options,
             note_context,
-            cur_acc,
             jobs,
         }
     }
@@ -157,7 +154,6 @@ impl<'a, 'd> SearchView<'a, 'd> {
                     self.txn,
                     self.is_muted,
                     self.note_context,
-                    self.cur_acc,
                     self.jobs,
                 )
                 .show(ui)

crates/notedeck_columns/src/timeline/route.rs
@@ -6,13 +6,12 @@ use crate::{
 };
 
 use enostr::Pubkey;
-use notedeck::{Accounts, MuteFun, NoteContext};
+use notedeck::{MuteFun, NoteContext};
 use notedeck_ui::{jobs::JobsCache, NoteOptions};
 
 #[allow(clippy::too_many_arguments)]
 pub fn render_timeline_route(
     timeline_cache: &mut TimelineCache,
-    accounts: &mut Accounts,
     kind: &TimelineKind,
     col: usize,
     note_options: NoteOptions,
@@ -32,10 +31,9 @@ pub fn render_timeline_route(
             let note_action = ui::TimelineView::new(
                 kind,
                 timeline_cache,
-                &accounts.mutefun(),
+                &note_context.accounts.mutefun(),
                 note_context,
                 note_options,
-                &(&accounts.get_selected_account().key).into(),
                 jobs,
             )
             .ui(ui);
@@ -47,11 +45,10 @@ pub fn render_timeline_route(
             if depth > 1 {
                 render_profile_route(
                     pubkey,
-                    accounts,
                     timeline_cache,
                     col,
                     ui,
-                    &accounts.mutefun(),
+                    &note_context.accounts.mutefun(),
                     note_options,
                     note_context,
                     jobs,
@@ -61,10 +58,9 @@ pub fn render_timeline_route(
                 let note_action = ui::TimelineView::new(
                     kind,
                     timeline_cache,
-                    &accounts.mutefun(),
+                    &note_context.accounts.mutefun(),
                     note_context,
                     note_options,
-                    &(&accounts.get_selected_account().key).into(),
                     jobs,
                 )
                 .ui(ui);
@@ -78,7 +74,6 @@ pub fn render_timeline_route(
 #[allow(clippy::too_many_arguments)]
 pub fn render_thread_route(
     threads: &mut Threads,
-    accounts: &mut Accounts,
     selection: &ThreadSelection,
     col: usize,
     mut note_options: NoteOptions,
@@ -94,9 +89,8 @@ pub fn render_thread_route(
         threads,
         selection.selected_or_root(),
         note_options,
-        &accounts.mutefun(),
+        &note_context.accounts.mutefun(),
         note_context,
-        &(&accounts.get_selected_account().key).into(),
         jobs,
     )
     .id_source(col)
@@ -107,7 +101,6 @@ pub fn render_thread_route(
 #[allow(clippy::too_many_arguments)]
 pub fn render_profile_route(
     pubkey: &Pubkey,
-    accounts: &Accounts,
     timeline_cache: &mut TimelineCache,
     col: usize,
     ui: &mut egui::Ui,
@@ -118,7 +111,6 @@ pub fn render_profile_route(
 ) -> Option<RenderNavAction> {
     let profile_view = ProfileView::new(
         pubkey,
-        accounts,
         col,
         timeline_cache,
         note_options,
@@ -130,7 +122,8 @@ pub fn render_profile_route(
 
     if let Some(action) = profile_view {
         match action {
-            ui::profile::ProfileViewAction::EditProfile => accounts
+            ui::profile::ProfileViewAction::EditProfile => note_context
+                .accounts
                 .get_full(pubkey)
                 .map(|kp| RenderNavAction::ProfileAction(ProfileAction::Edit(kp.to_full()))),
             ui::profile::ProfileViewAction::Note(note_action) => {

commit ff0428550b73af1670d26b3873959f18f2588304

fix missing zap button
Changelog-Fixed: Fix missing zap button
Fixes: 397bfce8173c ("add `Accounts` to `NoteContext`")
Fixes: https://github.com/damus-io/notedeck/issues/1021
Signed-off-by: William Casarin <jb55@jb55.com>

crates/notedeck/src/account/accounts.rs
@@ -207,6 +207,10 @@ impl Accounts {
         self.cache.selected_mut()
     }
 
+    pub fn get_selected_wallet(&self) -> Option<&ZapWallet> {
+        self.cache.selected().wallet.as_ref()
+    }
+
     pub fn get_selected_wallet_mut(&mut self) -> Option<&mut ZapWallet> {
         self.cache.selected_mut().wallet.as_mut()
     }

crates/notedeck/src/lib.rs
@@ -72,8 +72,8 @@ pub use unknowns::{get_unknown_note_ids, NoteRefsUnkIdAction, SingleUnkIdAction,
 pub use urls::{supported_mime_hosted_at_url, SupportedMimeType, UrlMimes};
 pub use user_account::UserAccount;
 pub use wallet::{
-    get_current_wallet, get_wallet_for, GlobalWallet, Wallet, WalletError, WalletType,
-    WalletUIState, ZapWallet,
+    get_current_wallet, get_current_wallet_mut, get_wallet_for, GlobalWallet, Wallet, WalletError,
+    WalletType, WalletUIState, ZapWallet,
 };
 pub use zaps::{
     get_current_default_msats, AnyZapState, DefaultZapError, DefaultZapMsats, NoteZapTarget,

crates/notedeck/src/note/mod.rs
@@ -5,6 +5,7 @@ pub use action::{MediaAction, NoteAction, ScrollInfo, ZapAction, ZapTargetAmount
 pub use context::{BroadcastContext, ContextSelection, NoteContextSelection};
 
 use crate::Accounts;
+use crate::GlobalWallet;
 use crate::JobPool;
 use crate::Localization;
 use crate::UnknownIds;
@@ -20,6 +21,7 @@ use std::fmt;
 pub struct NoteContext<'d> {
     pub ndb: &'d Ndb,
     pub accounts: &'d Accounts,
+    pub global_wallet: &'d GlobalWallet,
     pub i18n: &'d mut Localization,
     pub img_cache: &'d mut Images,
     pub note_cache: &'d mut NoteCache,

crates/notedeck/src/wallet.rs
@@ -24,12 +24,23 @@ pub fn get_wallet_for<'a>(
     global_wallet.wallet.as_ref()
 }
 
-pub fn get_current_wallet<'a>(
+pub fn get_current_wallet_mut<'a>(
     accounts: &'a mut Accounts,
     global_wallet: &'a mut GlobalWallet,
 ) -> Option<&'a mut ZapWallet> {
     let Some(wallet) = accounts.get_selected_wallet_mut() else {
         return global_wallet.wallet.as_mut();
+    };
+
+    Some(wallet)
+}
+
+pub fn get_current_wallet<'a>(
+    accounts: &'a Accounts,
+    global_wallet: &'a GlobalWallet,
+) -> Option<&'a ZapWallet> {
+    let Some(wallet) = accounts.get_selected_wallet() else {
+        return global_wallet.wallet.as_ref();
     };
 
     Some(wallet)

crates/notedeck_columns/src/nav.rs
@@ -514,6 +514,7 @@ fn render_nav_body(
         unknown_ids: ctx.unknown_ids,
         clipboard: ctx.clipboard,
         i18n: ctx.i18n,
+        global_wallet: ctx.global_wallet,
     };
     match top {
         Route::Timeline(kind) => {

crates/notedeck_columns/src/ui/note/post.rs
@@ -802,6 +802,7 @@ mod preview {
             let mut note_context = NoteContext {
                 ndb: app.ndb,
                 accounts: app.accounts,
+                global_wallet: app.global_wallet,
                 img_cache: app.img_cache,
                 note_cache: app.note_cache,
                 zaps: app.zaps,

crates/notedeck_columns/src/ui/wallet.rs
@@ -1,6 +1,6 @@
 use egui::{vec2, CornerRadius, Layout};
 use notedeck::{
-    get_current_wallet, tr, Accounts, DefaultZapMsats, GlobalWallet, Localization,
+    get_current_wallet_mut, tr, Accounts, DefaultZapMsats, GlobalWallet, Localization,
     NotedeckTextStyle, PendingDefaultZapState, Wallet, WalletError, WalletUIState, ZapWallet,
 };
 
@@ -103,7 +103,7 @@ impl WalletAction {
             }
             WalletAction::SetDefaultZapSats(new_default) => 's: {
                 let sats = {
-                    let Some(wallet) = get_current_wallet(accounts, global_wallet) else {
+                    let Some(wallet) = get_current_wallet_mut(accounts, global_wallet) else {
                         break 's;
                     };
 
@@ -138,7 +138,7 @@ impl WalletAction {
                 global_wallet.save_wallet();
             }
             WalletAction::EditDefaultZaps => 's: {
-                let Some(wallet) = get_current_wallet(accounts, global_wallet) else {
+                let Some(wallet) = get_current_wallet_mut(accounts, global_wallet) else {
                     break 's;
                 };
 

crates/notedeck_dave/src/ui/dave.rs
@@ -220,6 +220,7 @@ impl<'a> DaveUi<'a> {
             unknown_ids: ctx.unknown_ids,
             clipboard: ctx.clipboard,
             i18n: ctx.i18n,
+            global_wallet: ctx.global_wallet,
         };
 
         let txn = Transaction::new(note_context.ndb).unwrap();

crates/notedeck_ui/src/note/mod.rs
@@ -13,9 +13,12 @@ use crate::{
 
 pub use contents::{render_note_contents, render_note_preview, NoteContents};
 pub use context::NoteContextButton;
+use notedeck::get_current_wallet;
 use notedeck::note::MediaAction;
 use notedeck::note::ZapTargetAmount;
 use notedeck::ui::is_narrow;
+use notedeck::Accounts;
+use notedeck::GlobalWallet;
 use notedeck::Images;
 use notedeck::Localization;
 pub use options::NoteOptions;
@@ -451,18 +454,13 @@ impl<'a, 'd> NoteView<'a, 'd> {
             note_action = contents.action.or(note_action);
 
             if self.options().contains(NoteOptions::ActionBar) {
-                let zapper = {
-                    let cur_acc = self.note_context.accounts.get_selected_account();
-                    let has_wallet = cur_acc.wallet.is_some();
-
-                    has_wallet.then_some(Zapper {
-                        zaps: self.note_context.zaps,
-                        cur_acc: cur_acc.keypair(),
-                    })
-                };
                 note_action = render_note_actionbar(
                     ui,
-                    zapper,
+                    get_zapper(
+                        self.note_context.accounts,
+                        self.note_context.global_wallet,
+                        self.note_context.zaps,
+                    ),
                     self.note.id(),
                     self.note.pubkey(),
                     note_key,
@@ -531,20 +529,14 @@ impl<'a, 'd> NoteView<'a, 'd> {
 
                 note_action = contents.action.or(note_action);
 
-                let zapper = {
-                    let cur_acc = self.note_context.accounts.get_selected_account();
-                    let has_wallet = cur_acc.wallet.is_some();
-
-                    has_wallet.then_some(Zapper {
-                        zaps: self.note_context.zaps,
-                        cur_acc: cur_acc.keypair(),
-                    })
-                };
-
                 if self.options().contains(NoteOptions::ActionBar) {
                     note_action = render_note_actionbar(
                         ui,
-                        zapper,
+                        get_zapper(
+                            self.note_context.accounts,
+                            self.note_context.global_wallet,
+                            self.note_context.zaps,
+                        ),
                         self.note.id(),
                         self.note.pubkey(),
                         note_key,
@@ -609,6 +601,20 @@ impl<'a, 'd> NoteView<'a, 'd> {
             .with_action(note_action)
             .with_pfp(note_ui_resp.pfp_rect)
     }
+}
+
+fn get_zapper<'a>(
+    accounts: &'a Accounts,
+    global_wallet: &'a GlobalWallet,
+    zaps: &'a Zaps,
+) -> Option<Zapper<'a>> {
+    let has_wallet = get_current_wallet(accounts, global_wallet).is_some();
+    let cur_acc = accounts.get_selected_account();
+
+    has_wallet.then_some(Zapper {
+        zaps,
+        cur_acc: cur_acc.keypair(),
+    })
 }
 
 fn get_reposted_note<'a>(ndb: &Ndb, txn: &'a Transaction, note: &Note) -> Option<Note<'a>> {
@@ -769,7 +775,7 @@ struct Zapper<'a> {
 #[profiling::function]
 fn render_note_actionbar(
     ui: &mut egui::Ui,
-    zapper: Option<Zapper>,
+    zapper: Option<Zapper<'_>>,
     note_id: &[u8; 32],
     note_pubkey: &[u8; 32],
     note_key: NoteKey,