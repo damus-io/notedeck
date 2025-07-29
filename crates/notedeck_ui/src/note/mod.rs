damus-io/notedeck

commit f2e01f0e4035130f228b9092bef8c204cef0f936

fix(note_actionbar): add invisible label to stabilize section width ¯\_(ツ)_/¯

crates/notedeck_ui/src/note/mod.rs
@@ -782,6 +782,7 @@ fn render_note_actionbar(
     i18n: &mut Localization,
 ) -> egui::InnerResponse<Option<NoteAction>> {
     ui.horizontal(|ui| {
+        ui.label(RichText::new("").text_style(egui::TextStyle::Small));
         ui.set_min_height(26.0);
         ui.spacing_mut().item_spacing.x = 24.0;
 

commit 2a85ee562c2c2b65bbfc0996754e328fe4d55e64

ui/note: simplify weird hack and make note of it
Fixes: https://github.com/damus-io/notedeck/issues/842
Fixes: f2e01f0e4035 ("fix(note_actionbar): add invisible label to stabilize section width ¯\_(ツ)_/¯")
Signed-off-by: William Casarin <jb55@jb55.com>

crates/notedeck_ui/src/note/mod.rs
@@ -783,7 +783,11 @@ fn render_note_actionbar(
     i18n: &mut Localization,
 ) -> egui::InnerResponse<Option<NoteAction>> {
     ui.horizontal(|ui| {
-        ui.label(RichText::new("").text_style(egui::TextStyle::Small));
+        // NOTE(jb55): without this we get a weird artifact where
+        // there subsequent lines start sinking leftward off the screen.
+        // question: WTF? question 2: WHY?
+        ui.allocate_space(egui::vec2(0.0, 0.0));
+
         ui.set_min_height(26.0);
         ui.spacing_mut().item_spacing.x = 24.0;
 