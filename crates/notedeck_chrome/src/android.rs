damus-io/notedeck

commit 9ff5753bcaead14d7d992eb03ee9ff96b93562a8

settings: use timed serializer, handle zoom properly, use custom text style for note body font size, added font size slider, added preview note

crates/notedeck_chrome/src/android.rs
@@ -69,7 +69,13 @@ pub async fn android_main(app: AndroidApp) {
         Box::new(move |cc| {
             let ctx = &cc.egui_ctx;
             let mut notedeck = Notedeck::new(ctx, path, &app_args);
-            setup_chrome(ctx, &notedeck.args(), notedeck.theme());
+            setup_chrome(
+                ctx,
+                &notedeck.args(),
+                notedeck.theme(),
+                notedeck.note_body_font_size(),
+                notedeck.zoom_factor(),
+            );
 
             let context = &mut notedeck.app_context();
             let dave = Dave::new(cc.wgpu_render_state.as_ref());

crates/notedeck_ui/src/note/contents.rs
@@ -4,7 +4,7 @@ use crate::{
 };
 use notedeck::{JobsCache, RenderableMedia};
 
-use egui::{vec2, Color32, Hyperlink, RichText};
+use egui::{vec2, Color32, Hyperlink, Label, RichText};
 use nostrdb::{BlockType, Mention, Note, NoteKey, Transaction};
 use tracing::warn;
 
@@ -42,6 +42,8 @@ impl<'a, 'd> NoteContents<'a, 'd> {
 
 impl egui::Widget for &mut NoteContents<'_, '_> {
     fn ui(self, ui: &mut egui::Ui) -> egui::Response {
+        ui.spacing_mut().item_spacing = vec2(0.0, 0.0);
+
         if self.options.contains(NoteOptions::ShowNoteClientTop) {
             render_client(ui, self.note_context.note_cache, self.note);
         }
@@ -200,13 +202,24 @@ pub fn render_note_contents<'a>(
                     }
 
                     _ => {
-                        ui.colored_label(link_color, format!("@{}", &block.as_str()[..16]));
+                        ui.colored_label(
+                            link_color,
+                            RichText::new(format!("@{}", &block.as_str()[..16]))
+                                .text_style(NotedeckTextStyle::NoteBody.text_style()),
+                        );
                     }
                 },
 
                 BlockType::Hashtag => {
+                    if block.as_str().trim().len() == 0 {
+                        continue 'block_loop;
+                    }
                     let resp = ui
-                        .colored_label(link_color, format!("#{}", block.as_str()))
+                        .colored_label(
+                            link_color,
+                            RichText::new(format!("#{}", block.as_str()))
+                                .text_style(NotedeckTextStyle::NoteBody.text_style()),
+                        )
                         .on_hover_cursor(egui::CursorIcon::PointingHand);
 
                     if resp.clicked() {
@@ -231,8 +244,13 @@ pub fn render_note_contents<'a>(
                     };
 
                     if hide_media || !found_supported() {
+                        if block.as_str().trim().len() == 0 {
+                            continue 'block_loop;
+                        }
                         ui.add(Hyperlink::from_label_and_url(
-                            RichText::new(block.as_str()).color(link_color),
+                            RichText::new(block.as_str())
+                                .color(link_color)
+                                .text_style(NotedeckTextStyle::NoteBody.text_style()),
                             block.as_str(),
                         ));
                     }
@@ -263,18 +281,18 @@ pub fn render_note_contents<'a>(
                     }
                     if options.contains(NoteOptions::ScrambleText) {
                         ui.add(
-                            egui::Label::new(
+                            Label::new(
                                 RichText::new(rot13(block_str))
-                                    .text_style(NotedeckTextStyle::Body.text_style()),
+                                    .text_style(NotedeckTextStyle::NoteBody.text_style()),
                             )
                             .wrap()
                             .selectable(selectable),
                         );
                     } else {
                         ui.add(
-                            egui::Label::new(
+                            Label::new(
                                 RichText::new(block_str)
-                                    .text_style(NotedeckTextStyle::Body.text_style()),
+                                    .text_style(NotedeckTextStyle::NoteBody.text_style()),
                             )
                             .wrap()
                             .selectable(selectable),

crates/notedeck/src/app.rs
@@ -1,13 +1,13 @@
 use crate::account::FALLBACK_PUBKEY;
 use crate::i18n::Localization;
-use crate::persist::{AppSizeHandler, ZoomHandler};
+use crate::persist::{AppSizeHandler, SettingsHandler};
 use crate::wallet::GlobalWallet;
 use crate::zaps::Zaps;
+use crate::JobPool;
 use crate::{
     frame_history::FrameHistory, AccountStorage, Accounts, AppContext, Args, DataPath,
     DataPathType, Directory, Images, NoteAction, NoteCache, RelayDebugView, UnknownIds,
 };
-use crate::{JobPool, SettingsHandler};
 use egui::Margin;
 use egui::ThemePreference;
 use egui_winit::clipboard::Clipboard;
@@ -40,9 +40,8 @@ pub struct Notedeck {
     global_wallet: GlobalWallet,
     path: DataPath,
     args: Args,
-    settings_handler: SettingsHandler,
+    settings: SettingsHandler,
     app: Option<Rc<RefCell<dyn App>>>,
-    zoom: ZoomHandler,
     app_size: AppSizeHandler,
     unrecognized_args: BTreeSet<String>,
     clipboard: Clipboard,
@@ -99,7 +98,15 @@ impl eframe::App for Notedeck {
 
         render_notedeck(self, ctx);
 
-        self.zoom.try_save_zoom_factor(ctx);
+        self.settings.update_batch(|settings| {
+            settings.zoom_factor = ctx.zoom_factor();
+            settings.locale = self.i18n.get_current_locale().to_string();
+            settings.theme = if ctx.style().visuals.dark_mode {
+                ThemePreference::Dark
+            } else {
+                ThemePreference::Light
+            };
+        });
         self.app_size.try_save_app_size(ctx);
 
         if self.args.relay_debug {
@@ -159,7 +166,7 @@ impl Notedeck {
             1024usize * 1024usize * 1024usize * 1024usize
         };
 
-        let settings_handler = SettingsHandler::new(&path).load();
+        let settings = SettingsHandler::new(&path).load();
 
         let config = Config::new().set_ingester_threads(2).set_mapsize(map_size);
 
@@ -214,12 +221,8 @@ impl Notedeck {
 
         let img_cache = Images::new(img_cache_dir);
         let note_cache = NoteCache::default();
-        let zoom = ZoomHandler::new(&path);
-        let app_size = AppSizeHandler::new(&path);
 
-        if let Some(z) = zoom.get_zoom_factor() {
-            ctx.set_zoom_factor(z);
-        }
+        let app_size = AppSizeHandler::new(&path);
 
         // migrate
         if let Err(e) = img_cache.migrate_v0() {
@@ -234,7 +237,7 @@ impl Notedeck {
         let mut i18n = Localization::new();
 
         let setting_locale: Result<LanguageIdentifier, LanguageIdentifierError> =
-            settings_handler.locale().parse();
+            settings.locale().parse();
 
         if setting_locale.is_ok() {
             if let Err(err) = i18n.set_locale(setting_locale.unwrap()) {
@@ -261,9 +264,8 @@ impl Notedeck {
             global_wallet,
             path: path.clone(),
             args: parsed_args,
-            settings_handler,
+            settings,
             app: None,
-            zoom,
             app_size,
             unrecognized_args,
             frame_history: FrameHistory::default(),
@@ -290,7 +292,7 @@ impl Notedeck {
             global_wallet: &mut self.global_wallet,
             path: &self.path,
             args: &self.args,
-            settings_handler: &mut self.settings_handler,
+            settings: &mut self.settings,
             clipboard: &mut self.clipboard,
             zaps: &mut self.zaps,
             frame_history: &mut self.frame_history,
@@ -308,7 +310,15 @@ impl Notedeck {
     }
 
     pub fn theme(&self) -> ThemePreference {
-        self.settings_handler.theme()
+        self.settings.theme()
+    }
+
+    pub fn note_body_font_size(&self) -> f32 {
+        self.settings.note_body_font_size()
+    }
+
+    pub fn zoom_factor(&self) -> f32 {
+        self.settings.zoom_factor()
     }
 
     pub fn unrecognized_args(&self) -> &BTreeSet<String> {

crates/notedeck/src/context.rs
@@ -20,7 +20,7 @@ pub struct AppContext<'a> {
     pub global_wallet: &'a mut GlobalWallet,
     pub path: &'a DataPath,
     pub args: &'a Args,
-    pub settings_handler: &'a mut SettingsHandler,
+    pub settings: &'a mut SettingsHandler,
     pub clipboard: &'a mut Clipboard,
     pub zaps: &'a mut Zaps,
     pub frame_history: &'a mut FrameHistory,

crates/notedeck/src/fonts.rs
@@ -31,6 +31,7 @@ pub fn desktop_font_size(text_style: &NotedeckTextStyle) -> f32 {
         NotedeckTextStyle::Button => 13.0,
         NotedeckTextStyle::Small => 12.0,
         NotedeckTextStyle::Tiny => 10.0,
+        NotedeckTextStyle::NoteBody => 16.0,
     }
 }
 
@@ -46,6 +47,7 @@ pub fn mobile_font_size(text_style: &NotedeckTextStyle) -> f32 {
         NotedeckTextStyle::Button => 13.0,
         NotedeckTextStyle::Small => 12.0,
         NotedeckTextStyle::Tiny => 10.0,
+        NotedeckTextStyle::NoteBody => 13.0,
     }
 }
 

crates/notedeck/src/persist/mod.rs
@@ -1,12 +1,9 @@
 mod app_size;
 mod settings_handler;
-mod theme_handler;
 mod token_handler;
-mod zoom;
 
 pub use app_size::AppSizeHandler;
 pub use settings_handler::Settings;
 pub use settings_handler::SettingsHandler;
-pub use theme_handler::ThemeHandler;
+pub use settings_handler::DEFAULT_NOTE_BODY_FONT_SIZE;
 pub use token_handler::TokenHandler;
-pub use zoom::ZoomHandler;

crates/notedeck/src/persist/settings_handler.rs
@@ -1,18 +1,23 @@
 use crate::{
-    storage::{self, delete_file},
-    DataPath, DataPathType, Directory,
+    storage::delete_file, timed_serializer::TimedSerializer, DataPath, DataPathType, Directory,
 };
 use egui::ThemePreference;
 use serde::{Deserialize, Serialize};
 use tracing::{error, info};
 
 const THEME_FILE: &str = "theme.txt";
+const ZOOM_FACTOR_FILE: &str = "zoom_level.json";
 const SETTINGS_FILE: &str = "settings.json";
 
 const DEFAULT_THEME: ThemePreference = ThemePreference::Dark;
 const DEFAULT_LOCALE: &str = "es-US";
 const DEFAULT_ZOOM_FACTOR: f32 = 1.0;
 const DEFAULT_SHOW_SOURCE_CLIENT: &str = "hide";
+const DEFAULT_SHOW_REPLIES_NEWEST_FIRST: bool = false;
+#[cfg(any(target_os = "android", target_os = "ios"))]
+pub const DEFAULT_NOTE_BODY_FONT_SIZE: f32 = 13.0;
+#[cfg(not(any(target_os = "android", target_os = "ios")))]
+pub const DEFAULT_NOTE_BODY_FONT_SIZE: f32 = 16.0;
 
 fn deserialize_theme(serialized_theme: &str) -> Option<ThemePreference> {
     match serialized_theme {
@@ -23,72 +28,96 @@ fn deserialize_theme(serialized_theme: &str) -> Option<ThemePreference> {
     }
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, PartialEq, Clone)]
 pub struct Settings {
     pub theme: ThemePreference,
     pub locale: String,
     pub zoom_factor: f32,
     pub show_source_client: String,
     pub show_replies_newest_first: bool,
+    pub note_body_font_size: f32,
 }
 
 impl Default for Settings {
     fn default() -> Self {
-        // Use the same fallback theme as before
         Self {
             theme: DEFAULT_THEME,
             locale: DEFAULT_LOCALE.to_string(),
             zoom_factor: DEFAULT_ZOOM_FACTOR,
             show_source_client: DEFAULT_SHOW_SOURCE_CLIENT.to_string(),
-            show_replies_newest_first: false,
+            show_replies_newest_first: DEFAULT_SHOW_REPLIES_NEWEST_FIRST,
+            note_body_font_size: DEFAULT_NOTE_BODY_FONT_SIZE,
         }
     }
 }
 
 pub struct SettingsHandler {
     directory: Directory,
+    serializer: TimedSerializer<Settings>,
     current_settings: Option<Settings>,
 }
 
 impl SettingsHandler {
-    fn read_legacy_theme(&self) -> Option<ThemePreference> {
+    fn read_from_theme_file(&self) -> Option<ThemePreference> {
         match self.directory.get_file(THEME_FILE.to_string()) {
             Ok(contents) => deserialize_theme(contents.trim()),
             Err(_) => None,
         }
     }
 
-    fn migrate_to_settings_file(&mut self) -> Result<(), ()> {
+    fn read_from_zomfactor_file(&self) -> Option<f32> {
+        match self.directory.get_file(ZOOM_FACTOR_FILE.to_string()) {
+            Ok(contents) => serde_json::from_str::<f32>(&contents).ok(),
+            Err(_) => None,
+        }
+    }
+
+    fn migrate_to_settings_file(&mut self) -> bool {
+        let mut settings = Settings::default();
+        let mut migrated = false;
         // if theme.txt exists migrate
-        if let Some(theme_from_file) = self.read_legacy_theme() {
+        if let Some(theme_from_file) = self.read_from_theme_file() {
             info!("migrating theme preference from theme.txt file");
             _ = delete_file(&self.directory.file_path, THEME_FILE.to_string());
 
-            self.current_settings = Some(Settings {
-                theme: theme_from_file,
-                ..Settings::default()
-            });
-
-            self.save();
-
-            Ok(())
+            settings.theme = theme_from_file;
+            migrated = true;
         } else {
-            Err(())
+            info!("theme.txt file not found, using default theme");
+        };
+
+        // if zoom_factor.txt exists migrate
+        if let Some(zom_factor) = self.read_from_zomfactor_file() {
+            info!("migrating theme preference from zom_factor file");
+            _ = delete_file(&self.directory.file_path, ZOOM_FACTOR_FILE.to_string());
+
+            settings.zoom_factor = zom_factor;
+            migrated = true;
+        } else {
+            info!("zoom_factor.txt exists migrate file not found, using default zoom factor");
+        };
+
+        if migrated {
+            self.current_settings = Some(settings);
+            self.try_save_settings();
         }
+        migrated
     }
 
     pub fn new(path: &DataPath) -> Self {
         let directory = Directory::new(path.path(DataPathType::Setting));
-        let current_settings: Option<Settings> = None;
+        let serializer =
+            TimedSerializer::new(path, DataPathType::Setting, "settings.json".to_owned());
 
         Self {
             directory,
-            current_settings,
+            serializer,
+            current_settings: None,
         }
     }
 
     pub fn load(mut self) -> Self {
-        if self.migrate_to_settings_file().is_ok() {
+        if self.migrate_to_settings_file() {
             return self;
         }
 
@@ -114,22 +143,9 @@ impl SettingsHandler {
         self
     }
 
-    pub fn save(&self) {
-        let settings = self.current_settings.as_ref().unwrap();
-        match serde_json::to_string(settings) {
-            Ok(serialized) => {
-                if let Err(e) = storage::write_file(
-                    &self.directory.file_path,
-                    SETTINGS_FILE.to_string(),
-                    &serialized,
-                ) {
-                    error!("Could not save settings: {}", e);
-                } else {
-                    info!("Settings saved successfully");
-                }
-            }
-            Err(e) => error!("Failed to serialize settings: {}", e),
-        };
+    pub(crate) fn try_save_settings(&mut self) {
+        let settings = self.get_settings_mut().clone();
+        self.serializer.try_save(settings);
     }
 
     pub fn get_settings_mut(&mut self) -> &mut Settings {
@@ -141,7 +157,7 @@ impl SettingsHandler {
 
     pub fn set_theme(&mut self, theme: ThemePreference) {
         self.get_settings_mut().theme = theme;
-        self.save();
+        self.try_save_settings();
     }
 
     pub fn set_locale<S>(&mut self, locale: S)
@@ -149,12 +165,12 @@ impl SettingsHandler {
         S: Into<String>,
     {
         self.get_settings_mut().locale = locale.into();
-        self.save();
+        self.try_save_settings();
     }
 
     pub fn set_zoom_factor(&mut self, zoom_factor: f32) {
         self.get_settings_mut().zoom_factor = zoom_factor;
-        self.save();
+        self.try_save_settings();
     }
 
     pub fn set_show_source_client<S>(&mut self, option: S)
@@ -162,12 +178,17 @@ impl SettingsHandler {
         S: Into<String>,
     {
         self.get_settings_mut().show_source_client = option.into();
-        self.save();
+        self.try_save_settings();
     }
 
     pub fn set_show_replies_newest_first(&mut self, value: bool) {
         self.get_settings_mut().show_replies_newest_first = value;
-        self.save();
+        self.try_save_settings();
+    }
+
+    pub fn set_note_body_font_size(&mut self, value: f32) {
+        self.get_settings_mut().note_body_font_size = value;
+        self.try_save_settings();
     }
 
     pub fn update_batch<F>(&mut self, update_fn: F)
@@ -176,12 +197,12 @@ impl SettingsHandler {
     {
         let settings = self.get_settings_mut();
         update_fn(settings);
-        self.save();
+        self.try_save_settings();
     }
 
     pub fn update_settings(&mut self, new_settings: Settings) {
         self.current_settings = Some(new_settings);
-        self.save();
+        self.try_save_settings();
     }
 
     pub fn theme(&self) -> ThemePreference {
@@ -216,10 +237,17 @@ impl SettingsHandler {
         self.current_settings
             .as_ref()
             .map(|s| s.show_replies_newest_first)
-            .unwrap_or(false)
+            .unwrap_or(DEFAULT_SHOW_REPLIES_NEWEST_FIRST)
     }
 
     pub fn is_loaded(&self) -> bool {
         self.current_settings.is_some()
+    }
+
+    pub fn note_body_font_size(&self) -> f32 {
+        self.current_settings
+            .as_ref()
+            .map(|s| s.note_body_font_size)
+            .unwrap_or(DEFAULT_NOTE_BODY_FONT_SIZE)
     }
 }

crates/notedeck/src/persist/theme_handler.rs
@@ -1,76 +1,0 @@
-use egui::ThemePreference;
-use tracing::{error, info};
-
-use crate::{storage, DataPath, DataPathType, Directory};
-
-pub struct ThemeHandler {
-    directory: Directory,
-    fallback_theme: ThemePreference,
-}
-
-const THEME_FILE: &str = "theme.txt";
-
-impl ThemeHandler {
-    pub fn new(path: &DataPath) -> Self {
-        let directory = Directory::new(path.path(DataPathType::Setting));
-        let fallback_theme = ThemePreference::Dark;
-        Self {
-            directory,
-            fallback_theme,
-        }
-    }
-
-    pub fn load(&self) -> ThemePreference {
-        match self.directory.get_file(THEME_FILE.to_owned()) {
-            Ok(contents) => match deserialize_theme(contents) {
-                Some(theme) => theme,
-                None => {
-                    error!(
-                        "Could not deserialize theme. Using fallback {:?} instead",
-                        self.fallback_theme
-                    );
-                    self.fallback_theme
-                }
-            },
-            Err(e) => {
-                error!(
-                    "Could not read {} file: {:?}\nUsing fallback {:?} instead",
-                    THEME_FILE, e, self.fallback_theme
-                );
-                self.fallback_theme
-            }
-        }
-    }
-
-    pub fn save(&self, theme: ThemePreference) {
-        match storage::write_file(
-            &self.directory.file_path,
-            THEME_FILE.to_owned(),
-            &theme_to_serialized(&theme),
-        ) {
-            Ok(_) => info!(
-                "Successfully saved {:?} theme change to {}",
-                theme, THEME_FILE
-            ),
-            Err(_) => error!("Could not save {:?} theme change to {}", theme, THEME_FILE),
-        }
-    }
-}
-
-fn theme_to_serialized(theme: &ThemePreference) -> String {
-    match theme {
-        ThemePreference::Dark => "dark",
-        ThemePreference::Light => "light",
-        ThemePreference::System => "system",
-    }
-    .to_owned()
-}
-
-fn deserialize_theme(serialized_theme: String) -> Option<ThemePreference> {
-    match serialized_theme.as_str() {
-        "dark" => Some(ThemePreference::Dark),
-        "light" => Some(ThemePreference::Light),
-        "system" => Some(ThemePreference::System),
-        _ => None,
-    }
-}

commit 6a08d4b1b2ba8af6031fbd2d72628f50c9df6bfa

ui/note: fix width instabilities because of spacing_mut
TODO: get rid of all spacing_mut in the codebase

Fixes: 9ff5753bcaea ("settings: use timed serializer, handle zoom properly...")

crates/notedeck_chrome/src/android.rs
@@ -10,7 +10,7 @@ use notedeck::Notedeck;
 #[tokio::main]
 pub async fn android_main(app: AndroidApp) {
     //use tracing_logcat::{LogcatMakeWriter, LogcatTag};
-    use tracing_subscriber::{EnvFilter, prelude::*};
+    use tracing_subscriber::{prelude::*, EnvFilter};
 
     std::env::set_var("RUST_BACKTRACE", "full");
     //std::env::set_var("DAVE_ENDPOINT", "http://ollama.jb55.com/v1");

crates/notedeck_ui/src/note/contents.rs
@@ -4,7 +4,7 @@ use crate::{
 };
 use notedeck::{JobsCache, RenderableMedia};
 
-use egui::{vec2, Color32, Hyperlink, Label, RichText};
+use egui::{Color32, Hyperlink, Label, RichText};
 use nostrdb::{BlockType, Mention, Note, NoteKey, Transaction};
 use tracing::warn;
 
@@ -42,8 +42,6 @@ impl<'a, 'd> NoteContents<'a, 'd> {
 
 impl egui::Widget for &mut NoteContents<'_, '_> {
     fn ui(self, ui: &mut egui::Ui) -> egui::Response {
-        ui.spacing_mut().item_spacing = vec2(0.0, 0.0);
-
         if self.options.contains(NoteOptions::ShowNoteClientTop) {
             render_client(ui, self.note_context.note_cache, self.note);
         }
@@ -159,8 +157,6 @@ pub fn render_note_contents<'a>(
             ui.weak(note.content());
             return;
         };
-
-        ui.spacing_mut().item_spacing = vec2(0.0, 0.0);
 
         'block_loop: for block in blocks.iter(note) {
             match block.blocktype() {