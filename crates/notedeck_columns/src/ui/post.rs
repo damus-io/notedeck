a8f45971bb24d46a200ddda459e343b58d422de5
Martti Malmi <sirius@iki.fi>
2025-11-06 22:17:49 +0200

    more neutral colored post button

--- crates/notedeck_columns/src/ui/post.rs
+++ crates/notedeck_columns/src/ui/post.rs
@@ -20,10 +20,10 @@ pub fn compose_note_button(dark_mode: bool) -> impl Widget {
         let use_line_width = helper.scale_1d_pos(min_line_width);
         let use_edge_circle_radius = helper.scale_radius(min_line_width);
 
-        // TODO: theme!?
-        let fill_color = notedeck_ui::colors::PINK;
+        let text_color = ui.visuals().text_color();
+        let bg_color = ui.visuals().extreme_bg_color;
 
-        painter.circle_filled(helper.center(), use_background_radius, fill_color);
+        painter.circle_filled(helper.center(), use_background_radius, text_color);
 
         let min_half_plus_sign_size = min_plus_sign_size / 2.0;
         let north_edge = helper.scale_from_center(0.0, min_half_plus_sign_size);
@@ -31,27 +31,21 @@ pub fn compose_note_button(dark_mode: bool) -> impl Widget {
         let west_edge = helper.scale_from_center(-min_half_plus_sign_size, 0.0);
         let east_edge = helper.scale_from_center(min_half_plus_sign_size, 0.0);
 
-        let icon_color = if !dark_mode {
-            Color32::BLACK
-        } else {
-            Color32::WHITE
-        };
-
         painter.line_segment(
             [north_edge, south_edge],
-            Stroke::new(use_line_width, icon_color),
+            Stroke::new(use_line_width, bg_color),
         );
         painter.line_segment(
             [west_edge, east_edge],
-            Stroke::new(use_line_width, icon_color),
+            Stroke::new(use_line_width, bg_color),
         );
-        painter.circle_filled(north_edge, use_edge_circle_radius, icon_color);
-        painter.circle_filled(south_edge, use_edge_circle_radius, icon_color);
-        painter.circle_filled(west_edge, use_edge_circle_radius, icon_color);
-        painter.circle_filled(east_edge, use_edge_circle_radius, icon_color);
+        painter.circle_filled(north_edge, use_edge_circle_radius, bg_color);
+        painter.circle_filled(south_edge, use_edge_circle_radius, bg_color);
+        painter.circle_filled(west_edge, use_edge_circle_radius, bg_color);
+        painter.circle_filled(east_edge, use_edge_circle_radius, bg_color);
 
         helper
             .take_animation_response()
-            .on_hover_text("Compose new note")
+            .on_hover_text("New note")
     }
 }
