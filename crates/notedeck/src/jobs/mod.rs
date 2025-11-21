28c364c4b162eb3b95bfa8f810169f1c1167a57d
kernelkind <kernelkind@gmail.com>
2025-11-21 13:40:34 -0700

    feat(jobs): structs for new JobCache
    Signed-off-by: kernelkind <kernelkind@gmail.com>

--- crates/notedeck/src/jobs/mod.rs
+++ crates/notedeck/src/jobs/mod.rs
@@ -1,6 +1,10 @@
 mod cache_old;
 mod job_pool;
+pub(crate) mod types;
 
+pub use crate::jobs::types::{
+    CompleteResponse, JobOutput, JobPackage, JobRun, NoOutputRun, RunType,
+};
 pub use cache_old::{
     BlurhashParams, Job, JobError, JobIdOld, JobParams, JobParamsOwned, JobState, JobsCacheOld,
 };
