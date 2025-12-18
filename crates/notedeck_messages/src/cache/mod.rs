53631727d6b773fd11cd4e9c9083648dabc539c7
kernelkind <kernelkind@gmail.com>
2025-12-18 17:50:53 -0500

    feat(convo-cache): add `ConversationCache`
    Signed-off-by: kernelkind <kernelkind@gmail.com>

--- crates/notedeck_messages/src/cache/mod.rs
+++ crates/notedeck_messages/src/cache/mod.rs
@@ -1,7 +1,11 @@
+mod conversation;
 mod message_store;
 mod registry;
 mod state;
 
+pub use conversation::{
+    Conversation, ConversationCache, ConversationListState, ConversationMetadata,
+};
 pub use message_store::{MessageStore, NotePkg};
 pub use registry::{
     ConversationId, ConversationIdentifier, ConversationIdentifierUnowned, ParticipantSetUnowned,
