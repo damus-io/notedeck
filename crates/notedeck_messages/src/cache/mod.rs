61b7ac12c453a9527254206bafad4abd637397fb
kernelkind <kernelkind@gmail.com>
2025-12-18 17:18:21 -0500

    feat(convo-state): add `ConversationStates`
    for mutable access to certain data from UI
    
    Signed-off-by: kernelkind <kernelkind@gmail.com>

--- crates/notedeck_messages/src/cache/mod.rs
+++ crates/notedeck_messages/src/cache/mod.rs
@@ -1,7 +1,9 @@
 mod message_store;
 mod registry;
+mod state;
 
 pub use message_store::{MessageStore, NotePkg};
 pub use registry::{
     ConversationId, ConversationIdentifier, ConversationIdentifierUnowned, ParticipantSetUnowned,
 };
+pub use state::{ConversationState, ConversationStates};
