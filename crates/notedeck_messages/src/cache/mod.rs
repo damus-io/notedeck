bafd1adc948b255fa56c522235729924c72810ff
kernelkind <kernelkind@gmail.com>
2025-12-18 17:17:09 -0500

    feat(message-store): add MessageStore
    Signed-off-by: kernelkind <kernelkind@gmail.com>

--- crates/notedeck_messages/src/cache/mod.rs
+++ crates/notedeck_messages/src/cache/mod.rs
@@ -1,5 +1,7 @@
+mod message_store;
 mod registry;
 
+pub use message_store::{MessageStore, NotePkg};
 pub use registry::{
     ConversationId, ConversationIdentifier, ConversationIdentifierUnowned, ParticipantSetUnowned,
 };
